6479b8d037879ba9aa31825174d6e475761187ea
qnhanhh <caoquynhanh18@gmail.com>
2022-09-04 23:40:45 +0700

    update pathway

--- src/Pathway.js
+++ src/Pathway.js
@@ -5,7 +5,7 @@ const Pathway = () => {
         <div className='pathway-container'>
             <h1 className='explain-title'>lộ trình ôn thi thptqg môn sinh học</h1>
             <h6 className='explain-subtitle'>ôn sớm - luyện sâu - bứt phá 9+</h6>
-            <div className='explain-desc'>
+            <div className='pathway-desc'>
             </div>
             <div className="banner-register center-btn">
                 <a className='banner-link hvr-bounce-out' href='#registration'>Đăng ký ngay</a>
